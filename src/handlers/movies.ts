b34ffe13ebb98e9b3efd14a7e8121d006f67c501
Tijani <tijaniusat4barcelona@gmail.com>
2022-01-30 18:34:26 +0100

    Get A Movie

--- src/handlers/movies.ts
+++ src/handlers/movies.ts
@@ -3,9 +3,20 @@ import { movies } from '../data/movies'
 import { BaseHandler } from '../interfaces/handler'
 
 class Movies extends BaseHandler {
-    // Get all movies
+  // Get all movies
   static getAllMovies(req: Request, res: Response) {
     return res.status(200).send({ movies })
+  }
+
+  // Get a movie
+  static getAMovie(req: Request, res: Response) {
+    const movieId = Number(req.params.id)
+    const mov = movies.find((item) => item.id === movieId)
+    if (mov) {
+      return res.status(200).send(mov)
+    } else {
+      return res.status(404).send({ message: 'MOVIES NOT FOUND!' })
+    }
   }
 }
 
