d7595d1764e6436d72ce78ee47fe3e4a2827a719
Tijani <tijaniusat4barcelona@gmail.com>
2022-01-30 18:37:14 +0100

    Get A Movie

--- src/handlers/movies.ts
+++ src/handlers/movies.ts
@@ -3,6 +3,7 @@ import { movies } from '../data/movies'
 import { BaseHandler } from '../interfaces/handler'
 
 class Movies extends BaseHandler {
+  
   // Get all movies
   static getAllMovies(req: Request, res: Response) {
     return res.status(200).send({ movies })
