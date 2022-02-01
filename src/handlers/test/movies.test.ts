a95cada3b2e72d9eb101ac2a3b0797b74e91b533
Tijani <tijaniusat4barcelona@gmail.com>
2022-02-01 05:42:08 +0100

    Delete test endpoint

--- src/handlers/test/movies.test.ts
+++ src/handlers/test/movies.test.ts
@@ -13,7 +13,7 @@ const newMovie = {
   language: 'Chinese',
   release_date: 1997
 }
-
+// test to add a movie
 describe('POST /movies/add', () => {
   it('should Add movie', (done) => {
     request
@@ -23,6 +23,30 @@ describe('POST /movies/add', () => {
         expect(res.status).to.be.equal(201)
         expect(res.body).to.be.an('object')
         expect(res.body.message).be.equal('Movie added successfully')
+        done()
+      })
+  })
+})
+
+// test to delete a movie
+describe('DELETE /movies/:id', () => {
+  it('should delete a movie', (done) => {
+    request
+      .delete('/api/movies/1')
+      .expect(204)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(204)
+        expect(res.body).to.be.an('object')
+        done()
+      })
+  })
+  it('should display a message if movie not found', (done) => {
+    request
+      .delete('/api/movies/10000')
+      .expect(404)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(404)
+        expect(res.body.message).to.be.equal('Movie does not exist!')
         done()
       })
   })
