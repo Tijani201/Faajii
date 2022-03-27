f529c248b215ddaaa35fc11cd7059d69b85645e0
Tijani <tijaniusat4barcelona@gmail.com>
2022-03-27 16:55:46 +0100

    Another fix2

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,7 @@ const router = express.Router()
 
 /* GET home page. */
 const welcomeRoute = (req: Request, res: Response) => {
-  res.status(200).send({ title: 'Welcome to Faaji API' })
+  res.status(200).send({ title: 'Welcome to Faaji Entertainment API' })
 }
 
 router.get('/', welcomeRoute)
