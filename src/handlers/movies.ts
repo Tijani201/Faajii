342631d7e6967cb68c9117b2a7277a4d7f701633
Tijani <tijaniusat4barcelona@gmail.com>
2022-06-23 20:06:10 +0100

    Get A Movies

--- src/handlers/movies.ts
+++ src/handlers/movies.ts
@@ -7,6 +7,17 @@ class Movies extends BaseHandler {
   static getAllMovies(req: Request, res: Response) {
     return res.status(200).send({ movies })
   }
+
+  // Get a movie
+  static getAMovie(req: Request, res: Response) {
+    const movieId = Number(req.params.id)
+    const mov = movies.find((item) => item.id === movieId)
+    if (mov) {
+      return res.status(200).send(mov)
+    } else {
+      return res.status(404).send({ message: 'MOVIES NOT FOUND!' })
+    }
+  }
 }
 
 export default Movies
--- src/routes/movies.ts
+++ src/routes/movies.ts
@@ -5,5 +5,7 @@ const router = express.Router()
 
 /* Route to get all movies */
 router.get('/movies', movies.getAllMovies)
+/* Route to get a school by id */
+router.get('/movies/:id', movies.getAMovie)
 
 export default router
