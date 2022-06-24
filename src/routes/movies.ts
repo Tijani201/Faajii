26a68d3fb2900f68bf216baf02fc02ace96d43c9
Tijani <tijaniusat4barcelona@gmail.com>
2022-06-24 19:29:33 +0100

    Add A Movie

--- src/routes/movies.ts
+++ src/routes/movies.ts
@@ -7,5 +7,7 @@ const router = express.Router()
 router.get('/movies', movies.getAllMovies)
 /* Route to get a school by id */
 router.get('/movies/:id', movies.getAMovie)
+/* Route to add a school */
+router.post('/movies/add', movies.addAMovie)
 
 export default router
